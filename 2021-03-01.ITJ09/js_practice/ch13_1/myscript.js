dd7146aa7aa6c5ea28178baf5a14cd8638bb8e87
zdrivemf57 <zdrivemf57@gmail.com>
2022-08-22 11:46:25 +0900

    init

---
+++ 2021-03-01.ITJ09/js_practice/ch13_1/myscript.js
@@ -0,0 +1,9 @@
+/*
+  合計金額を表示させる
+*/
+let price = 100; //値段の変数
+let unit = 3; //個数
+let total = price * unit; //合計
+let msg = "合計" + total + "円";
+
+alert(msg); //表示
---
+++ 2021-06-01.ITJ10/html_practice/ch13_1/message.js
@@ -0,0 +1,14 @@
+/*
+ こんにちはと表示
+*/
+// 変数の利用
+let msg = "こんにちは";
+console.log(msg);
+
+msg = 100;
+console.log(msg);
+
+const name = "山田太郎";
+
+name = "佐藤";
+console.log(name);
