43d61787db45fb18ba3ebb1a4e90936d7a0666cd
zdrivemf57 <zdrivemf57@gmail.com>
2025-01-31 16:17:37 +0900

    職業訓練_Javaコース_2025の続き

--- 2025.ITJ/11.JavaScript_advanced/practice02-own/promise-cgpt-own.js
+++ 2025.ITJ/11.JavaScript_advanced/practice02-own/promise-cgpt-own.js
@@ -2,14 +2,14 @@ const promise = new Promise((resolve, reject) => {
   let success = true; // 成功する場合の例
   // let success = false; // 成功する場合の例
   if (success) {
-    resolve('成功しました！');
+    resolve('成功しました！'); // resolveの引数は成功した時の戻り値となる
   } else {
-    reject('失敗しました…');
+    reject('失敗しました…'); // rejectの引数は失敗した時の戻り値となる
   }
 });
 
 promise
-  .then((resolveResult) => {
+  .then((resolveResult) => { // promiseの評価をresolveResultで受け取る
     console.log('成功: ', resolveResult); // resolveで渡された値を受け取る
   })
   .catch((rejectResult) => {
